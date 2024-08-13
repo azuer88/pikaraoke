c20f8e073996fdac2ceebc9ff0576fa83a219b51
Blue Cuenca <blue.cuenca@gmail.com>
2024-08-14 02:14:22 +0800

    add logo video
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- karaoke.py
+++ karaoke.py
@@ -422,6 +422,7 @@ class Karaoke:
         if fr.cdg_file_path is not None:  # handle CDG files
             if fr.cdg_file_path == "":
                 # stream_ready_string = "no matching"
+                logging.debug(f"Using image '{self.logo_path}' as video")
                 video = ffmpeg.input(self.logo_path, loop=1, tune="stillimage")
                 output = ffmpeg.output(audio, video, ffmpeg_url,
                                        vcodec="libx264", pix_fmt="yuv420p", video_bitrate="500k",
