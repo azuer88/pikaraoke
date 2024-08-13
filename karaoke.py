d331e0d7696f0d73caeca044bf4dcedeaae808cb
Blue Cuenca <blue.cuenca@gmail.com>
2024-08-14 01:37:54 +0800

    test mp3
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- karaoke.py
+++ karaoke.py
@@ -421,10 +421,10 @@ class Karaoke:
 
         if fr.cdg_file_path is not None:  # handle CDG files
             if fr.cdg_file_path == "":
-                stream_ready_string = "Audio: mp3"
+                stream_ready_string = "Not found in anywhere"
                 output = ffmpeg.output(audio, ffmpeg_url,
-                                       acodec="copy", preset="ultrafast",
-                                       listen=1, f="mp3")
+                                       acodec="aac", preset="ultrafast",
+                                       listen=1, f="m4a")
             else:
                 logging.info("Playing CDG/MP3 file: " + file_path)
                 # Ffmpeg outputs "Video: cdgraphics" when the stream is ready to consume
