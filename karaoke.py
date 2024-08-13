73eb17ccd8d91b697f3860b1803d103e6e7e6ef8
Blue Cuenca <blue.cuenca@gmail.com>
2024-08-14 02:06:50 +0800

    add logo video
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- karaoke.py
+++ karaoke.py
@@ -424,7 +424,7 @@ class Karaoke:
                 # stream_ready_string = "no matching"
                 video = ffmpeg.input(self.logo_path, loop=1, tune="stillimage")
                 output = ffmpeg.output(audio, video, ffmpeg_url,
-                                       vcodec="libx264", pix_fmt="yuv420p", vidio_bitrate="500k",
+                                       vcodec="libx264", pix_fmt="yuv420p", video_bitrate="500k",
                                        movflags="frag_keyframe+default_base_moof",
                                        acodec="copy", preset="ultrafast",
                                        shortest=True,
