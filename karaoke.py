32adfe3ec1c4fa33802fea49af263c739f4ce3e4
Blue Cuenca <blue.cuenca@gmail.com>
2024-08-14 17:15:07 +0800

    use premade image for video generation
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- karaoke.py
+++ karaoke.py
@@ -423,10 +423,11 @@ class Karaoke:
             if fr.cdg_file_path == "":
                 # stream_ready_string = "no matching"
                 logging.debug(f"Using image '{self.logo_path}' as video")
+                target = Path(self.logo_path).with_suffix('.jpg')
                 video = ffmpeg.input(self.logo_path, loop=1, framerate=30)
                 audio = ffmpeg.input(fr.file_path)
                 output = ffmpeg.output(audio, video, ffmpeg_url,
-                                       vcodec="libx264", pix_fmt="yuv420p", video_bitrate="500k",
+                                       vcodec="libx264", pix_fmt="yuv420p10le", video_bitrate="500k",
                                        movflags="frag_keyframe+default_base_moof",
                                        acodec="aac", preset="ultrafast",
                                        shortest=None, tune="stillimage",
