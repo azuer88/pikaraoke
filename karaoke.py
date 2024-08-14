ba4bbe319c4879a1ce2c60cb00b718ee96d68c4a
Blue Cuenca <blue.cuenca@gmail.com>
2024-08-14 17:32:02 +0800

    use premade image file to create stillimage video
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- karaoke.py
+++ karaoke.py
@@ -424,7 +424,7 @@ class Karaoke:
                 # stream_ready_string = "no matching"
                 logging.debug(f"Using image '{self.logo_path}' as video")
                 target = Path(self.logo_path).with_suffix('.jpg')
-                video = ffmpeg.input(target.as_posix(), loop=1, framerate=30, pix_fmt="yuv420p")
+                video = ffmpeg.input(target.as_posix(), loop=1)
                 audio = ffmpeg.input(fr.file_path)
                 output = ffmpeg.output(audio, video, ffmpeg_url,
                                        vcodec="libx264", pix_fmt="yuv420p", video_bitrate="500k",
